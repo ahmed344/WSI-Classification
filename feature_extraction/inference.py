ac58808b7c479fa031a2726281ebed8e0e055c23
ahmed344 <ahmedsharaf483@gmail.com>
2025-12-09 15:36:40 +0000

    Exclude the tiles out of tissue

--- feature_extraction/inference.py
+++ feature_extraction/inference.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import torch
+import cv2
 import openslide
 import timm
 from torchvision import transforms
@@ -14,7 +15,7 @@ from dataset import WSI_tiles
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
-path = "/workspaces/WSI-Classification/data/HE-MYO/Processed/Dystrophic"
+path = "/workspaces/WSI-Classification/data/HE-MYO/Processed/Inflammatory"
 TILE_SIZE= (448, 448)
 BATCH_SIZE = 64
 
@@ -25,7 +26,7 @@ slides = os.listdir(path)
 # model = timm.create_model("hf-hub:bioptimus/H-optimus-0", pretrained=True, init_values=1e-5, dynamic_img_size=False)
 
 # Load the whole model
-model = torch.load("H-Optimus-0.pth", map_location=device, weights_only=False)
+model = torch.load("/workspaces/WSI-Classification/models/H-Optimus-0.pth", map_location=device, weights_only=False)
 
 # Put the model on the device
 model = model.to(device)
@@ -46,25 +47,41 @@ transform = transforms.Compose([
 for slide_name in slides:
     # Check if the embedding features already exist
     if os.path.exists(f"{path}/{slide_name.split('.')[0]}_features.pkl"):
-        print(f"{slide_name.split('.')[0]} already processid")
+        print(f"{slide_name.split('.')[0]} already processed")
         continue
     
     # Print the slide name
     print(f"Processing {slide_name}")
 
     # Load the slide with openslide
-    slide = openslide.OpenSlide(f"/workspaces/WSI-Classification/data/HE-MYO/Processed/Dystrophic/{slide_name}")
+    slide = openslide.OpenSlide(f"{path}/{slide_name}")
+
+    # Read the slide as RGB
+    img = slide.read_region((0, 0), 0, slide.dimensions).convert("RGB")
+
+    # Convert RGB to HSV for color segmentation
+    hsv_img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2HSV)
+
+    # Threshold to segment tissue (non-white)
+    lower = (0, 10, 0)
+    upper = (180, 255, 220)
+    mask = cv2.inRange(hsv_img, lower, upper)
 
     # Calculate tile centers for 448x448 tiles without intersection
+    delta = (TILE_SIZE[0] // 2, TILE_SIZE[1] // 2)
     slide_width, slide_height = slide.dimensions
     x_coords, y_coords = [], []
-    for y in range(TILE_SIZE[1]//2, slide_height, TILE_SIZE[1]):
-        for x in range(TILE_SIZE[0]//2, slide_width, TILE_SIZE[0]):
-            x_coords.append(x)
-            y_coords.append(y)
+    for y in range(delta[1], slide_height, TILE_SIZE[1]):
+        for x in range(delta[0], slide_width, TILE_SIZE[0]):
+            if np.median(mask[y-delta[1]:y+delta[1], x-delta[0]:x+delta[0]]) > 0:
+                x_coords.append(x)
+                y_coords.append(y)
 
     # Create DataFrame with id, x, and y columns
     tiles = pd.DataFrame({'id': range(len(x_coords)), 'x': x_coords, 'y': y_coords})
+
+    # Save the pixels
+    tiles.to_csv(f"{path}/{slide_name.split('.')[0]}_tiles.csv")
 
     # Create the dataset
     dataset = WSI_tiles(slide=slide, tiles=tiles, transform=transform, size=TILE_SIZE)
@@ -83,19 +100,13 @@ for slide_name in slides:
             output = model(batch.to(device))
         
         # Append the embedding to the list
-        feature_emb.append(output.cpu().numpy())
+        feature_emb.append(output.cpu())
 
-    # Concatenate the embeddings
-    feature_emb = np.concatenate(feature_emb)
+    # Concatenate the embeddings into a single tensor
+    feature_emb = torch.cat(feature_emb, dim=0)
 
-    # Transform the embedding features into a dataframe
-    feature_emb_df = pd.DataFrame(feature_emb, index=tiles.index)
+    # Save the embedding features as a tensor using torch.save
+    torch.save(feature_emb, f"{path}/{slide_name.split('.')[0]}_features.pt")
 
     # Clean up the GPU memory
     torch.cuda.empty_cache()
-
-    # Save the pixels
-    tiles.to_pickle(f"{path}/{slide_name.split('.')[0]}_tiles.pkl")
-
-    # Save the embedding features to a pickle file
-    feature_emb_df.to_pickle(f"{path}/{slide_name.split('.')[0]}_features.pkl")
